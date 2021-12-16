ab0e04bdcb42ad88cfa402a7bea3213b3992f9e1
Sergey Suchkov <suseal@inbox.ru>
2021-12-16 14:49:47 +0700

    Change the top text of the menu

--- sokoban/Game.cs
+++ sokoban/Game.cs
@@ -60,7 +60,7 @@ namespace Sokoban
 
         private static void PrintMenu()
         {
-            var topText = "Choose a level:";
+            var topText = "Choose a map:";
             var bottomText = $"{KeyMap[Keys.Left]} - previous, {KeyMap[Keys.Right]} - next," +
                             $"\n{KeyMap[Keys.Choose]} - choose, {KeyMap[Keys.Exit]} - exit";
 
